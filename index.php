b6c400add1757b53a04e4f36eb127f8a460eb3ed
Prpsh17 <pasha17102005@gmail.com>
2025-05-04 11:02:28 +0700

    tambah file index php biar ada nama

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$name= "Pasha";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Kita</title>
+</head>
+<body>
+    <h1>Nama saya <?php echo $name ?></h1>
+</body>
+</html>
